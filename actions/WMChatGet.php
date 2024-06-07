331c7fc71df41d5c05dc67723a20826577e7dcda
Andrejs Verza <andrejs.verza@zabbix.com>
2024-06-07 09:01:52 -0300

    Task 4

---
+++ actions/WMChatGet.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\WMChat\Actions;
+
+use API,
+	CController,
+	CControllerResponseData;
+
+class WMChatGet extends CController {
+
+	public function init(): void {
+		$this->disableCsrfValidation();
+	}
+
+	protected function checkInput(): bool {
+		return true;
+	}
+
+	protected function checkPermissions(): bool {
+		return true;
+	}
+
+	protected function doAction(): void {
+		$history_data = API::History()->get([
+			'output' => ['clock', 'value'],
+			'history' => ITEM_VALUE_TYPE_STR,
+			'itemids' => '47201',
+			'sortfield' => 'clock',
+			'sortorder' => 'ASC'
+		]);
+
+		$output = [];
+
+		$output['chat_messages'] = array_map(
+			static function (array $history_entry): array {
+				[
+					'author' => $author,
+					'message' => $message
+				] = json_decode($history_entry['value'], true);
+
+				return [
+					'author' => $author,
+					'message' => $message,
+					'time' => date(ZBX_FULL_DATE_TIME, $history_entry['clock'])
+				];
+			},
+			$history_data
+		);
+
+		$this->setResponse(new CControllerResponseData(['main_block' => json_encode($output)]));
+	}
+}
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,13 @@
 class WMChatView {
 
 	/**
+	 * Container of chat messages.
+	 *
+	 * @type {HTMLDivElement}
+	 */
+	messages_container;
+
+	/**
 	 * New message text input.
 	 *
 	 * @type {HTMLInputElement}
@@ -15,6 +22,10 @@ class WMChatView {
 	submit_button;
 
 	constructor() {
+		this.messages_container = document.getElementById('wmchat-messages');
+
+		this.startUpdatingMessages();
+
 		const input_container = document.getElementById('wmchat-input');
 
 		this.input = input_container.querySelector('input');
@@ -26,6 +37,47 @@ class WMChatView {
 
 		this.submit_button = input_container.querySelector('button');
 		this.submit_button.addEventListener('click', () => this.submitMessage());
+	}
+
+	startUpdatingMessages() {
+		const curl = new Curl('zabbix.php');
+		curl.setArgument('action', 'wmchat.get');
+
+		fetch(curl.getUrl())
+			.then(response => response.json())
+			.then(response => {
+		 		this.setMessages(response.chat_messages);
+			})
+			.finally(() => {
+				setTimeout(() => this.startUpdatingMessages(), 1000);
+			});
+	}
+
+	setMessages(messages) {
+		this.messages_container.innerHTML = '';
+
+		messages.forEach(message => this.appendMessage(message));
+	}
+
+	appendMessage({author, message, time}) {
+		const container_time = document.createElement('div');
+		container_time.classList.add('time');
+		container_time.textContent = time;
+
+		const container_author = document.createElement('span');
+		container_author.classList.add('author');
+		container_author.textContent = author;
+
+		const container_message = document.createElement('span');
+		container_message.classList.add('message');
+		container_message.textContent = message;
+
+		const container_author_message = document.createElement('div');
+		container_author_message.appendChild(container_author);
+		container_author_message.appendChild(container_message);
+
+		this.messages_container.appendChild(container_time);
+		this.messages_container.appendChild(container_author_message);
 	}
 
 	submitMessage() {
